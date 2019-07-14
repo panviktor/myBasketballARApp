1090b3ced1c2b27199e59eda57966e38841e1b6a
Panviktor <panafviktor@gmail.com>
2019-07-14 04:49:38 +0300

    SCNPhysicsContactDelegate

---
+++ myBasketballARApp/Model/BitMaskCategory.swift
@@ -0,0 +1,19 @@
+//
+//  BitMaskCategory.swift
+//  myBasketballARApp
+//
+//  Created by Виктор on 14/07/2019.
+//  Copyright © 2019 SwiftViktor. All rights reserved.
+//
+
+
+struct BitMaskCategory: OptionSet {
+    let rawValue: Int
+    
+//    static let none = BitMaskCategory(rawValue: 1 << 0)
+    static let backboard  = BitMaskCategory(rawValue: 1 << 1)
+//    static let hope = BitMaskCategory(rawValue: 1 << 2)
+    static let boll = BitMaskCategory(rawValue: 1 << 3)
+    static let topHoop = BitMaskCategory(rawValue: 1 << 4)
+    static let bottonHoop = BitMaskCategory(rawValue: 1 << 5)
+}
--- myBasketballARApp/View/ViewControllers/GameViewController.swift
+++ myBasketballARApp/View/ViewControllers/GameViewController.swift
@@ -10,8 +10,21 @@ import ARKit
 
 class GameViewController: UIViewController  {
     
+    // MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
     
+    // MARK: - Properties
+    var isBackboardPlaced = false {
+        didSet {
+            if isBackboardPlaced {
+                guard let configuration = sceneView.session.configuration as? ARWorldTrackingConfiguration else { return }
+                configuration.planeDetection = []
+                sceneView.session.run(configuration)
+            }
+        }
+    }
+    
+    // MARK: - Overide UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
@@ -30,15 +43,21 @@ class GameViewController: UIViewController  {
         sceneView.session.pause()
     }
     
+    // MARK: - IBAction Methods
     @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
-        let touchLocation = sender.location(in: sceneView)
-        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-        if let nearestResult = hitTestResult.first {
-            addBackboard(result: nearestResult)
+        if isBackboardPlaced {
+            addBall()
+        } else {
+            let touchLocation = sender.location(in: sceneView)
+            let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            if let nearestResult = hitTestResult.first {
+                addBackboard(result: nearestResult)
+                sceneView.scene.physicsWorld.contactDelegate = self
+                isBackboardPlaced = true
+            }
         }
     }
 }
-
 
 // MARK: - GameViewController Custom Method
 extension GameViewController {
@@ -62,11 +81,43 @@ extension GameViewController {
         backboard.simdTransform = result.worldTransform
         backboard.eulerAngles.x -= .pi / 2
         sceneView.scene.rootNode.addChildNode(backboard)
+        let shape = SCNPhysicsShape(node: backboard, options: [
+            SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron
+        ])
+        let body = SCNPhysicsBody(type: .static, shape: shape)
+        backboard.physicsBody = body
+        backboard.physicsBody?.categoryBitMask = BitMaskCategory.backboard.rawValue
+        backboard.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
+        backboard.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
+        
+        
         sceneView.scene.rootNode.enumerateChildNodes { node, _ in
             if node.name == "Wall" {
                 node.removeFromParentNode()
             }
         }
+    }
+    
+    private func addBall() {
+        guard let frame = sceneView.session.currentFrame else { return }
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.24))
+        ball.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(
+            node: ball,
+            options: [SCNPhysicsShape.Option.collisionMargin: 0.01] //TRY 0.0001
+        ))
+        ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/ball.jpg")
+        
+        let transform = SCNMatrix4(frame.camera.transform)
+        ball.transform = transform
+        
+        let power: Float = 10
+        let force = SCNVector3(-transform.m31 * power, -transform.m32 * power, -transform.m33 * power)
+        ball.physicsBody?.applyForce(force, asImpulse: true)
+        ball.physicsBody?.categoryBitMask = BitMaskCategory.boll.rawValue
+        ball.physicsBody?.collisionBitMask = BitMaskCategory.backboard.rawValue
+        ball.physicsBody?.contactTestBitMask = BitMaskCategory.backboard.rawValue
+        
+        sceneView.scene.rootNode.addChildNode(ball)
     }
 }
 
@@ -78,4 +129,20 @@ extension GameViewController: ARSCNViewDelegate {
         let wall = createWall(planeAnchor: planeAnchor)
         node.addChildNode(wall)
     }
+}
+
+
+// MARK: - SCNPhysicsContactDelegate
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let nodeA = contact.nodeA //backboard
+//        let nodeB = contact.nodeB //boll
+        
+        if nodeA.physicsBody?.contactTestBitMask == BitMaskCategory.backboard.rawValue {
+            print(#function, #line)
+            
+        }
+    }
+    
 }
