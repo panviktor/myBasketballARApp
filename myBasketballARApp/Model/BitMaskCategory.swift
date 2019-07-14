0d53327d37bd755d84b997bfd91d24e98bfe6bad
Panviktor <panafviktor@gmail.com>
2019-07-15 01:40:36 +0300

    My Basketball AR

--- myBasketballARApp/Model/BitMaskCategory.swift
+++ myBasketballARApp/Model/BitMaskCategory.swift
@@ -10,10 +10,12 @@
 struct BitMaskCategory: OptionSet {
     let rawValue: Int
     
-//    static let none = BitMaskCategory(rawValue: 1 << 0)
-    static let backboard  = BitMaskCategory(rawValue: 1 << 1)
-//    static let hope = BitMaskCategory(rawValue: 1 << 2)
-    static let boll = BitMaskCategory(rawValue: 1 << 3)
-    static let topHoop = BitMaskCategory(rawValue: 1 << 4)
-    static let bottonHoop = BitMaskCategory(rawValue: 1 << 5)
+    static let none = BitMaskCategory(rawValue: 1 << 0)
+    static let boll = BitMaskCategory(rawValue: 1 << 1)
+    static let backboard  = BitMaskCategory(rawValue: 1 << 2)
+    static let bottonHoop = BitMaskCategory(rawValue: 1 << 3)
+    static let middleHoop = BitMaskCategory(rawValue: 1 << 4)
+    static let topHoop = BitMaskCategory(rawValue: 1 << 5)
+    static let worldBox = BitMaskCategory(rawValue: 1 << 6)
 }
+
--- myBasketballARApp/View/ViewControllers/GameViewController.swift
+++ myBasketballARApp/View/ViewControllers/GameViewController.swift
@@ -12,6 +12,8 @@ class GameViewController: UIViewController  {
     
     // MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet var goalsLabel: UILabel!
+    @IBOutlet var throwsLabel: UILabel!
     
     // MARK: - Properties
     var isBackboardPlaced = false {
@@ -24,11 +26,20 @@ class GameViewController: UIViewController  {
         }
     }
     
+    private var goals = 0
+    private var trows = 0
+    
+    private var contactTopHoop = false
+    private var contactBottonHoop = false
+    
     // MARK: - Overide UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
         sceneView.showsStatistics = true
+        
+        goalsLabel.text = "Goals: 0"
+        throwsLabel.text = "Throws: 0"
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -77,20 +88,95 @@ extension GameViewController {
     }
     
     private func addBackboard(result: ARHitTestResult) {
-        let backboard = SCNScene(named: "art.scnassets/backboard.scn")!.rootNode.clone()
-        backboard.simdTransform = result.worldTransform
-        backboard.eulerAngles.x -= .pi / 2
-        sceneView.scene.rootNode.addChildNode(backboard)
-        let shape = SCNPhysicsShape(node: backboard, options: [
-            SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron
-        ])
-        let body = SCNPhysicsBody(type: .static, shape: shape)
-        backboard.physicsBody = body
+        let backboardCSN = SCNScene(named: "art.scnassets/backboard.scn")
+        
+        guard let mainNode = backboardCSN?.rootNode.childNode(withName: "mainNode", recursively: false) else { return }
+        mainNode.simdTransform = result.worldTransform
+        mainNode.eulerAngles.x -= .pi / 2
+        
+        guard let backboard = mainNode.childNode(withName: "backboard", recursively: false) else { return }
+        guard let topHoop = backboard.childNode(withName: "topHoop", recursively: false) else { return }
+        guard let middleHoop = backboard.childNode(withName: "middleHoop", recursively: false) else { return }
+        guard let bottonHoop = backboard.childNode(withName: "bottonHoop", recursively: false) else { return }
+        guard let worldBox = mainNode.childNode(withName: "worldBox", recursively: false) else { return }
+        
+        let backboardShape = SCNPhysicsShape(node: backboard, options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron])
+        let backboardPhysicsBody = SCNPhysicsBody(type: .static, shape: backboardShape)
+        backboard.physicsBody = backboardPhysicsBody
         backboard.physicsBody?.categoryBitMask = BitMaskCategory.backboard.rawValue
         backboard.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
         backboard.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
         
+        let topHoopShape = SCNPhysicsShape(node: topHoop, options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron])
+        let topHoppePhysicsBody = SCNPhysicsBody(type: .static, shape: topHoopShape)
+        topHoop.physicsBody = topHoppePhysicsBody
+        topHoop.physicsBody?.categoryBitMask = BitMaskCategory.topHoop.rawValue
+        topHoop.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
+        topHoop.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
+        topHoop.opacity = 0
         
+        let middleHoopShape = SCNPhysicsShape(node: middleHoop, options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron])
+        let middleHoopPhysicsBody = SCNPhysicsBody(type: .static, shape: middleHoopShape)
+        middleHoop.physicsBody = middleHoopPhysicsBody
+        middleHoop.physicsBody?.categoryBitMask = BitMaskCategory.bottonHoop.rawValue
+        middleHoop.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
+        middleHoop.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
+        
+        let bottonHoopShape = SCNPhysicsShape(node: bottonHoop, options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron])
+        let bottonHoopPhysicsBody = SCNPhysicsBody(type: .static, shape: bottonHoopShape)
+        bottonHoop.physicsBody = bottonHoopPhysicsBody
+        bottonHoop.physicsBody?.categoryBitMask = BitMaskCategory.bottonHoop.rawValue
+        bottonHoop.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
+        bottonHoop.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
+        bottonHoop.opacity = 0
+        
+        let worldBoxShape = SCNPhysicsShape(node: worldBox, options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron])
+        let worldBoxPhysicsBody = SCNPhysicsBody(type: .static, shape: worldBoxShape)
+        worldBox.physicsBody = worldBoxPhysicsBody
+        worldBox.physicsBody?.categoryBitMask = BitMaskCategory.worldBox.rawValue
+        worldBox.physicsBody?.collisionBitMask = BitMaskCategory.boll.rawValue
+        worldBox.physicsBody?.contactTestBitMask = BitMaskCategory.boll.rawValue
+        worldBox.opacity = 0
+        
+        sceneView.scene.rootNode.addChildNode(mainNode)
+        hideWall()
+    }
+    
+    private func contactHoopReset() {
+        contactTopHoop = false
+        contactBottonHoop = false
+    }
+    
+    private func addBall() {
+        guard let frame = sceneView.session.currentFrame else { return }
+        
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.24))
+        ball.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(
+            node: ball,
+            options: [SCNPhysicsShape.Option.collisionMargin: 0.005]
+        ))
+        ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/ball.jpg")
+        
+        let transform = SCNMatrix4(frame.camera.transform)
+        ball.transform = transform
+        ball.name = "ball"
+        
+        let power: Float = 10
+        let force = SCNVector3(-transform.m31 * power, -transform.m32 * power, -transform.m33 * power)
+        
+        ball.physicsBody?.applyForce(force, asImpulse: true)
+        ball.physicsBody?.categoryBitMask = BitMaskCategory.boll.rawValue
+        ball.physicsBody?.collisionBitMask = BitMaskCategory.backboard.rawValue
+        ball.physicsBody?.contactTestBitMask = BitMaskCategory.backboard.rawValue
+        
+        contactHoopReset()
+    
+        sceneView.scene.rootNode.addChildNode(ball)
+        trows += 1
+        throwsLabel.text = "Throws: " + String(trows)
+    }
+    
+    private func hideWall() {
         sceneView.scene.rootNode.enumerateChildNodes { node, _ in
             if node.name == "Wall" {
                 node.removeFromParentNode()
@@ -98,27 +184,6 @@ extension GameViewController {
         }
     }
     
-    private func addBall() {
-        guard let frame = sceneView.session.currentFrame else { return }
-        let ball = SCNNode(geometry: SCNSphere(radius: 0.24))
-        ball.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(
-            node: ball,
-            options: [SCNPhysicsShape.Option.collisionMargin: 0.01] //TRY 0.0001
-        ))
-        ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/ball.jpg")
-        
-        let transform = SCNMatrix4(frame.camera.transform)
-        ball.transform = transform
-        
-        let power: Float = 10
-        let force = SCNVector3(-transform.m31 * power, -transform.m32 * power, -transform.m33 * power)
-        ball.physicsBody?.applyForce(force, asImpulse: true)
-        ball.physicsBody?.categoryBitMask = BitMaskCategory.boll.rawValue
-        ball.physicsBody?.collisionBitMask = BitMaskCategory.backboard.rawValue
-        ball.physicsBody?.contactTestBitMask = BitMaskCategory.backboard.rawValue
-        
-        sceneView.scene.rootNode.addChildNode(ball)
-    }
 }
 
 // MARK: - ARSCNViewDelegate
@@ -136,13 +201,30 @@ extension GameViewController: ARSCNViewDelegate {
 
 extension GameViewController: SCNPhysicsContactDelegate {
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        let nodeA = contact.nodeA //backboard
-//        let nodeB = contact.nodeB //boll
+        if contact.nodeA.physicsBody?.categoryBitMask == BitMaskCategory.boll.rawValue && contact.nodeB.physicsBody?.categoryBitMask == BitMaskCategory.topHoop.rawValue && !contactTopHoop {
+            contactTopHoop = true
+            contactBottonHoop = false
+        }
         
-        if nodeA.physicsBody?.contactTestBitMask == BitMaskCategory.backboard.rawValue {
-            print(#function, #line)
-            
+        if contact.nodeA.physicsBody?.categoryBitMask == BitMaskCategory.boll.rawValue && contact.nodeB.physicsBody?.categoryBitMask == BitMaskCategory.worldBox.rawValue {
+            DispatchQueue.main.async {
+                contact.nodeA.removeFromParentNode()
+            }
         }
     }
     
+    func physicsWorld(_ world: SCNPhysicsWorld, didEnd contact: SCNPhysicsContact) {
+        if contactTopHoop && !contactBottonHoop {
+//  true false????????????       if  contact.nodeA.physicsBody?.categoryBitMask == BitMaskCategory.boll.rawValue && contact.nodeB.physicsBody?.categoryBitMask == BitMaskCategory.bottonHoop.rawValue {
+           if contact.nodeA.physicsBody?.categoryBitMask == BitMaskCategory.boll.rawValue && contact.nodeB.name == "bottonHoop" {
+              print(#line,  contact.nodeB.physicsBody?.categoryBitMask == BitMaskCategory.bottonHoop.rawValue, contact.nodeB.name == "bottonHoop")
+                contactBottonHoop = true
+                goals += 1
+                contactHoopReset()
+                DispatchQueue.main.async {
+                    self.goalsLabel.text = "Goals: \(self.goals)"
+                }
+            }
+        }
+    }
 }
